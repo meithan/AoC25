01509808d3caea2b7227215ef895bfb7453ffa52
Meithan West <meithan@gmail.com>
2025-12-01 12:27:29 -0600

    Add boilerplate

---
+++ boilerplate.py
@@ -0,0 +1,27 @@
+# Day XX: 
+
+import sys
+CLR = "\33[2K\r"
+
+# ==============================================================================
+
+# Get filename of input as CLI arg. If not given, default to "input.txt"
+if len(sys.argv) == 1:
+  input_fname = "input.txt"
+else:
+  input_fname = sys.argv[1]
+
+# Read and parse input
+with open(input_fname) as f:
+  for line in f:
+    pass
+
+# ------------------------------------------------------------------------------
+# Part 1
+
+print("Part 1:", None)
+
+# ------------------------------------------------------------------------------
+# Part 2
+
+print("Part 2:", None)
