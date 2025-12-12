1581a49a2e2cc4c5189fc797c34efe9b82e78c46
Meithan West <meithan@gmail.com>
2025-12-12 08:17:39 -0600

    Day 12 solution

--- day12/day12.py
+++ day12/day12.py
@@ -40,17 +40,24 @@ with open(input_fname) as f:
 # Areas of each present
 areas = [sum(row.count("#") for row in present) for present in presents]
 
-# Count how many test cases are impossible because the total
-# area of the presents is larger than the region's
+# Count how many test cases are either trivially possible because there's
+# enough space to fit all presents without any overlap, or impossible because
+# the total area of the presents is larger than the region's
+trivially_possible_count = 0
 impossible_count = 0
 for size, numbers in regions:
+  # Trivially possible
+  if size[0]//3 * size[1]//3 >= sum(numbers):
+    trivially_possible_count += 1
+  # Impossible
   region_area = size[0] * size[1]
   presents_area = sum([numbers[i]*areas[i] for i in range(len(numbers))])
   if presents_area > region_area:
     impossible_count += 1
 
-# Maybe?
-print("Part 1:", len(regions)-impossible_count)
+# If this accounts for all test cases, then we're done
+if trivially_possible_count + impossible_count == len(regions):
+  print("Part 1:", trivially_possible_count)
 
 # ------------------------------------------------------------------------------
 # Part 2
